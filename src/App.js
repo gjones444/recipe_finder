7af2df4888040501c50eb9224ef6e1927b15bd8c
Gjones444 <gjonesrei@gmail.com>
2021-11-08 22:37:20 -0500

    Separated Header into individual component

--- src/App.js
+++ src/App.js
@@ -1,48 +1,13 @@
 import "./App.css";
 import styled from "styled-components";
+import { Header, Logo, AppNameComponent, SearchComponent, SearchIcon, SearchInput } from "./components/Header";
 
-const Container = styled.div``;
-const Header = styled.div`
-  color: white;
-  background-color: black;
-  display: flex;
-  flex-direction: row;
-  padding: 20px;
-  font-size: 25px;
-  font-weight: bold;
-  box-shadow: 0 3px 6px 0 #555;
-  justify-content: space-between;
-`;
-const Logo = styled.div`
-  width: 36px;
-  height: 36px;
-  margin: 15px;
-`;
-const AppNameComponent = styled.div`
-  display: flex;
-  align-items: center;
-`;
-const SearchComponent = styled.div`
-  display: flex;
-  flex-direction: row;
-  background-color: white;
-  padding: 10px;
-  border-radius: 6px;
-  width: 50%;
-  height: 50%;
-  margin-top: 15px;
+const Container = styled.div`
+display: flex;
+flex-direction: column;
 `;
 
-const SearchIcon = styled.div`
-  width: 32px;
-  height: 32px;
-`;
-const SearchInput = styled.input`
-  border: none;
-  outline: none;
-  margin-left: 15px;
-  font-size: 16px;
-`;
+const RecipeListContainer = styled.div``
 
 function App() {
   return (
@@ -53,7 +18,7 @@ function App() {
           <Logo></Logo>
         </AppNameComponent>
         <SearchComponent>
-          <SearchIcon src="search-icon.svg" />
+          <SearchIcon src="../public/search-icon.svg" />
           <SearchInput placeholder="Search Recipe" />
         </SearchComponent>
       </Header>
---
+++ src/components/Header.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import styled from "styled-components";
+
+export const Header = styled.div`
+  color: white;
+  background-color: black;
+  display: flex;
+  flex-direction: row;
+  padding: 20px;
+  font-size: 25px;
+  font-weight: bold;
+  box-shadow: 0 3px 6px 0 #555;
+  justify-content: space-between;
+`;
+export const Logo = styled.div`
+  width: 36px;
+  height: 36px;
+  margin: 15px;
+`;
+export const AppNameComponent = styled.div`
+  display: flex;
+  align-items: center;
+`;
+export const SearchComponent = styled.div`
+  display: flex;
+  flex-direction: row;
+  background-color: white;
+  padding: 10px;
+  border-radius: 6px;
+  width: 50%;
+  height: 50%;
+  margin-top: 5px;
+`;
+
+export const SearchIcon = styled.div`
+  width: 32px;
+  height: 32px;
+`;
+export const SearchInput = styled.input`
+  border: none;
+  outline: none;
+  margin-left: 15px;
+  font-size: 16px;
+`;
+
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Navbar
